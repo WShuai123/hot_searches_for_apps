146e46006f354dd058f0b4ae90a9da3d2101ac06
iiecho <41373040+WShuai123@users.noreply.github.com>
2023-12-16 18:51:09 +0800

    Update hot_to_csv.py

--- hot_to_csv.py
+++ hot_to_csv.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     api_data = get_api_data(api_url)
     
     if api_data:
-        for key in list(api_data['result'].keys())[1:-1]:
+        for key in list(api_data['result'].keys())[1:]:
             print(key)
             indexs = [item["index"] for item in api_data['result']['%s' % key]]
 
